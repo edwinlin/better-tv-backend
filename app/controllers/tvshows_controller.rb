98508d8a0449e8557f3b99122e89cbaf6aedda72
NorbertLam <noobertlm@gmail.com>
2019-03-05 13:51:39 -0500

    tvshows controller

---
+++ app/controllers/tvshows_controller.rb
@@ -0,0 +1,29 @@
+class TvshowsController < ApplicationController
+
+  def index
+    @tvshows = Tvshow.all
+    render json: @tvshows
+  end
+
+  def show
+    @tvshow = Tvshow.find(params[:id])
+    render json: @tvshow
+  end
+
+  def create
+    @tvshow = Tvshow.create(tvshow_params)
+    render json: @tvshow
+  end
+
+  def update
+    @tvshow = Tvshow.find(params[:id])
+    @tvshow.update(tvshow_params)
+    render json: @tvshow
+  end
+
+  def destroy
+    @tvshow = Tvshow.find(params[:id])
+    @tvshow.destroy()
+  end
+
+end
